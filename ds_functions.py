1415d094c04abc776184be71ccd2ff6256f7ef5f
João David <joaodavid1999@hotmail.com>
2020-10-01 19:58:49 +0100

    First commit

---
+++ ds_functions.py
@@ -0,0 +1,149 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import warnings
+import sklearn.metrics as metrics
+import config as cfg
+import datetime as dt
+
+
+mdates._reset_epoch_test_example()
+mdates.set_epoch('0000-12-31T00:00:00')  # old epoch (pre MPL 3.3)
+
+warnings.simplefilter("ignore")
+NR_COLUMNS: int = 3
+HEIGHT: int = 4
+
+
+def choose_grid(nr):
+    if nr < NR_COLUMNS:
+        return 1, nr
+    else:
+        return (nr // NR_COLUMNS, NR_COLUMNS) if nr % NR_COLUMNS == 0 else (nr // NR_COLUMNS + 1, NR_COLUMNS)
+
+
+def set_axes(xvalues: list, ax: plt.Axes = None, title: str = '', xlabel: str = '', ylabel: str = '', percentage=False):
+    if ax is None:
+        ax = plt.gca()
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if percentage:
+        ax.set_ylim(0.0, 1.0)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_xticklabels(xvalues, fontsize='small', ha='center')
+
+    return ax
+
+
+def set_locators(xvalues: list, ax: plt.Axes = None):
+    if isinstance(xvalues[0], dt.datetime):
+        locator = mdates.AutoDateLocator()
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(locator, defaultfmt='%Y-%m-%d'))
+    else:
+        ax.set_xticks(xvalues)
+        ax.set_xlim(xvalues[0], xvalues[-1])
+
+    return ax
+
+
+def plot_line(xvalues: list, yvalues: list, ax: plt.Axes = None, title: str = '', xlabel: str = '',
+              ylabel: str = '', percentage=False):
+    ax = set_axes(xvalues, ax=ax, title=title, xlabel=xlabel, ylabel=ylabel, percentage=percentage)
+    ax = set_locators(xvalues, ax=ax)
+    ax.plot(xvalues,  yvalues, c=cfg.LINE_COLOR)
+
+
+def multiple_line_chart(xvalues: list, yvalues: dict, ax: plt.Axes = None, title: str = '',
+                        xlabel: str = '', ylabel: str = '', percentage=False):
+    ax = set_axes(xvalues, ax=ax, title=title, xlabel=xlabel, ylabel=ylabel, percentage=percentage)
+    ax = set_locators(xvalues, ax=ax)
+
+    for name, y in yvalues.items():
+        ax.plot(xvalues, y, label=name)
+    ax.legend()
+
+
+def bar_chart(xvalues: list, yvalues: list, ax: plt.Axes = None, title: str = '',
+              xlabel: str = '', ylabel: str = '', percentage=False):
+    ax = set_axes(xvalues, ax=ax, title=title, xlabel=xlabel, ylabel=ylabel, percentage=percentage)
+    ax.bar(xvalues, yvalues, edgecolor=cfg.LINE_COLOR, color=cfg.FILL_COLOR)
+
+
+def multiple_bar_chart(xvalues: list, yvalues: dict, ax: plt.Axes = None, title: str = '',
+                       xlabel: str = '', ylabel: str = '', percentage=False):
+    ax = set_axes(xvalues, ax=ax, title=title, xlabel=xlabel, ylabel=ylabel, percentage=percentage)
+
+    x = np.arange(len(xvalues))  # the label locations
+
+    width = 0.8 / (len(xvalues)*len(yvalues))
+    # the width of the bars
+    step = width / len(xvalues)
+    i: int = 0
+    for metric in yvalues:
+        ax.bar(x + i*width, yvalues[metric], width=width, align='center', label=metric)
+        i += 1
+    ax.set_xticks(x + width/len(xvalues) - step/2)
+    ax.legend(fontsize='x-small', title_fontsize='small')
+
+
+def plot_confusion_matrix(cnf_matrix: np.ndarray, classes_names: np.ndarray,
+                          ax: plt.Axes = None, normalize: bool = False):
+    if ax is None:
+        ax = plt.gca()
+    if normalize:
+        total = cnf_matrix.sum(axis=1)[:, np.newaxis]
+        cm = cnf_matrix.astype('float') / total
+        title = "Normalized confusion matrix"
+    else:
+        cm = cnf_matrix
+        title = 'Confusion matrix'
+    np.set_printoptions(precision=2)
+    tick_marks = np.arange(0, len(classes_names), 1)
+    ax.set_title(title)
+    ax.set_ylabel('True label')
+    ax.set_xlabel('Predicted label')
+    ax.set_xticks(tick_marks)
+    ax.set_yticks(tick_marks)
+    ax.set_xticklabels(classes_names)
+    ax.set_yticklabels(classes_names)
+    ax.imshow(cm, interpolation='nearest', cmap=cfg.cmap_blues)
+
+    fmt = '.2f' if normalize else 'd'
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, format(cm[i, j], fmt), color='w', horizontalalignment="center")
+
+
+def plot_evaluation_results(labels: np.ndarray, trn_y, prd_trn, tst_y, prd_tst):
+    cnf_mtx_trn = metrics.confusion_matrix(trn_y, prd_trn, labels)
+    tn_trn, fp_trn, fn_trn, tp_trn = cnf_mtx_trn.ravel()
+    cnf_mtx_tst = metrics.confusion_matrix(tst_y, prd_tst, labels)
+    tn_tst, fp_tst, fn_tst, tp_tst = cnf_mtx_tst.ravel()
+
+    evaluation = {'Accuracy': [(tn_trn + tp_trn) / (tn_trn + tp_trn + fp_trn + fn_trn),
+                               (tn_tst + tp_tst) / (tn_tst + tp_tst + fp_tst + fn_tst)],
+                  'Recall': [tp_trn / (tp_trn + fn_trn), tp_tst / (tp_tst + fn_tst)],
+                  'Specificity': [tn_trn / (tn_trn + fp_trn), tn_tst / (tn_tst + fp_tst)],
+                  'Precision': [tp_trn / (tp_trn + fp_trn), tp_tst / (tp_tst + fp_tst)]}
+
+    fig, axs = plt.subplots(1, 2, figsize=(2 * HEIGHT, HEIGHT))
+    multiple_bar_chart(['Train', 'Test'], evaluation, ax=axs[0], title="Model's performance over Train and Test sets")
+    plot_confusion_matrix(cnf_mtx_tst, labels, ax=axs[1])
+
+
+def plot_roc_chart(models: dict, tstX: np.ndarray, tstY: np.ndarray, ax: plt.Axes = None, target: str = 'class'):
+    if ax is None:
+        ax = plt.gca()
+    ax.set_xlim(0.0, 1.0)
+    ax.set_ylim(0.0, 1.0)
+    ax.set_xlabel('FP rate')
+    ax.set_ylabel('TP rate')
+    ax.set_title('ROC chart for %s' % target)
+
+    ax.plot([0, 1], [0, 1], color='navy', label='random', linewidth=1, linestyle='--',  marker='')
+    for clf in models.keys():
+        metrics.plot_roc_curve(models[clf], tstX, tstY, ax=ax, marker='', linewidth=1)
+    ax.legend(loc="lower right")
