ba93c2d6b387a72a7487756f42dc83ab50662e8e
Gabriel Almeida <galmeida9@gmail.com>
2020-12-16 16:43:54 +0000

    profiling and transformation done

--- lab9+10/data.py
+++ lab9+10/data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 # Covid deaths data
-covid_data = pd.read_csv('covid19_pt.csv')
+covid_data = pd.read_csv('covid19_pt.csv', index_col='Date', sep=',', decimal='.',
+                   parse_dates=True, infer_datetime_format=True)
 covid_data_raw = covid_data.copy()
 
 # All deaths data
-all_data = pd.read_csv('deaths_pt.csv')
+all_data = pd.read_csv('deaths_pt.csv', index_col='start_date', sep=',', decimal='.',
+                   parse_dates=True, infer_datetime_format=True)
 all_data_raw = all_data.copy()
 
 def get_covid_data():
